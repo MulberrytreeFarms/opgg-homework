672eaed7d4cd77d5f39c1ee2315eb94e78aea76f
강성현 <Mulberrytree@Mulberrytree-MacBookPro.local>
2023-01-13 21:38:04 +0900

    feat: define layout components

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,6 +1,21 @@
 import { Outlet } from 'react-router-dom'
 
+import Header from '@/components/header'
+
+import { DefaultLayoutStyle, MainStyle } from './style'
+
 const Layout = () => {
-  return <Outlet />
+  return (
+    <DefaultLayoutStyle>
+      <Header>
+        <Header.Search>
+          <Header.SearchInput />
+        </Header.Search>
+      </Header>
+      <MainStyle>
+        <Outlet />
+      </MainStyle>
+    </DefaultLayoutStyle>
+  )
 }
 export default Layout
---
+++ src/layouts/style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+import { Container } from '@/styles/common'
+
+export const DefaultLayoutStyle = styled.div``
+
+export const MainStyle = styled.main`
+  ${Container};
+`
